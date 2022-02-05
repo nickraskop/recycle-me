0e38b51e086f525cd3a13a449ab8cfda8eeb1b52
Nick Raskop <raskop@usc.edu>
2022-02-05 01:24:19 -0800

    Setup page skeletons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Play from './pages/Play';
+import Leaderboard from './pages/Leaderboard';
+import Learn from './pages/Learn';
+import About from './pages/About';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Play />} />
+      <Route path="about" element={<Leaderboard />} />
+      <Route path="portfolio" element={<Learn />} />
+      <Route path="contact" element={<About />} />
+    </Routes>
   );
 }
 
---
+++ src/pages/Learn.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Learn() {
+  return (
+    <h1>Learn Placeholder</h1>
+  );
+}
