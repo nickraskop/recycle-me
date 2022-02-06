3210f0d75649a223452072acc5e251fccfb3c1b0
JonPhan02 <phanjona@usc.edu>
2022-02-05 18:51:12 -0800

    updated about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,7 +11,38 @@ export default function About() {
       </ul>
       <div id='inbody'>
         <h1>About</h1>
-        <p> This game tries to teach kids about how to recycle and what can/cannot be recycled. </p>
+        <br></br>
+        <p> <b>This game tries to teach kids about how to recycle and what can/cannot be recycled. We made it during Spring 2022 HackSC (2/5/2022)</b> </p>
+        <br></br>
+        <br></br>
+        <div class="info">
+          <div class="profile">
+            <div class="person">
+              <b>Jonathan Phan</b>
+              <a href="https://www.linkedin.com/in/jonathan-phan-csba/" target="_blank">
+                <img src="https://media-exp1.licdn.com/dms/image/C4D03AQFyacJuOsEyaQ/profile-displayphoto-shrink_400_400/0/1613551509676?e=1649894400&v=beta&t=U2-V1nvMIQanDbcLpj88XuAYwbNaYkiyHJ4hvmHgRgk" id="jon" width="200px" class ="zoom"></img>
+              </a>
+            </div>
+            <div class="person">
+              <b>Nick Raskop</b>
+              <a href="https://www.linkedin.com/in/nickraskop/" target="_blank">
+                <img src="https://media-exp1.licdn.com/dms/image/C5603AQF1UEirUpeE2w/profile-displayphoto-shrink_400_400/0/1632272931713?e=1649894400&v=beta&t=LxDhvxokDYnPZ3n24xHCL3p470rs4Kn-rKkFNqPrSLQ" id="nick" width="200px" class ="zoom"></img>
+              </a>
+            </div>
+            <div class="person">
+              <b>Wardah Jabeen</b>
+              <a href="https://www.linkedin.com/in/wardah-jabeen-9488b71ba/" target="_blank">
+                <img src="https://media-exp1.licdn.com/dms/image/C5603AQFFv35jOEWijA/profile-displayphoto-shrink_400_400/0/1643230336267?e=1649894400&v=beta&t=qAo51z0dNz__hcSL364MlQXDJXl-YfKS0Ajep4stdQc" id="wardeh" width="200px" class ="zoom"></img>
+              </a>
+            </div>
+            <div class="person">
+              <b>Kritin Dhoka</b>
+              <a href="https://www.linkedin.com/in/kritin-dhoka-ba76031bb/" target="_blank">
+                <img src="https://media-exp1.licdn.com/dms/image/C5603AQF-5LNdQppvHw/profile-displayphoto-shrink_400_400/0/1626262293528?e=1649894400&v=beta&t=maniG8kwiLUsRu_aL8qw652whmGxRqs8qdTrnvu1bWM" id="kritin" width="200px" class ="zoom"></img>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </body>
   );
--- src/pages/Play.js
+++ src/pages/Play.js
@@ -1,5 +1,35 @@
 import React from 'react';
 
+
+var trashCan = document.querySelector(".can");
+let move = 30;
+
+window.addEventListener("keyup", (e) => {
+  switch(e.key){
+    case "ArrowLeft":
+      trashCan.style.left = parseInt(trashCan.style.left) - move + "px";
+      break;
+    case "ArrowRight":
+      trashCan.style.left = parseInt(trashCan.style.left) + move + "px";
+      break;
+  }
+});
+window.addEventListener("load", () => {
+  trashCan.style.position = "absolute";
+  trashCan.style.left = 0;
+});
+
+window.addEventListener("keyup", (e) => {
+  switch(e.key){
+    case "ArrowLeft":
+      trashCan.style.left = parseInt(trashCan.style.left) - move + "px";
+      break;
+    case "ArrowRight":
+      trashCan.style.left = parseInt(trashCan.style.left) + move + "px";
+      break;
+  }
+});
+
 export default function Play() {
   return (
     <body>
@@ -11,6 +41,12 @@ export default function Play() {
     </ul>
     <div id='inbody'>
       <h1>Play</h1>
+    </div>
+    <img src="https://www.nicepng.com/png/detail/59-590244_plastic-bottles-clipart-full-water-bottle-clipart-png.png" width="100px" height="auto"></img>
+    <img src="https://www.pinclipart.com/picdir/middle/15-156478_big-image-clip-art-plastic-bag-png-download.png" width="100px" height="auto"></img>
+    <img src="https://www.pngitem.com/pimgs/m/55-554059_transparent-background-banana-peel-cartoon-hd-png-download.png" width="100px" height="auto"></img>
+    <div class="can">
+
     </div>
   </body>
   );
