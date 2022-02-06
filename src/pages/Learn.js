d13fb8929425e6bafb2343e1d8933ded8f93173f
wardah-jabeen <jabeen@usc.edu>
2022-02-06 02:59:23 -0800

    edited

--- src/pages/Learn.js
+++ src/pages/Learn.js
@@ -16,13 +16,11 @@ export default function Learn() {
       
       <h1>Learn</h1>
       
-{/* 
+
     <section>
       <div>
       <h2> Learn About Recycling</h2>
       </div>
-   
-    <p>Recycling is good for the environemnt as it saves resources and prevents pollution. </p>
    
 
     <table>
@@ -43,7 +41,7 @@ export default function Learn() {
         <td>glass of any color is typically recyclable. Keep broken glass out of your curbside bin for the safety of sanitation workers.</td>
       </tr>
       <tr>
-        <td>Plastic containers:</td>
+        <td>Plastic containers</td>
         <td>containers labeled #1 through #7 are commonly accepted, but make sure they’re clean.</td>
       </tr>
       <tr>
@@ -57,7 +55,7 @@ export default function Learn() {
     </section>
     <section>
     <h2> Learn About Trash</h2>
-    <p>detail</p>
+
    
 
     <table>
@@ -66,47 +64,24 @@ export default function Learn() {
         <th>Examples</th>
       </tr>
       <tr>
-        <td>Coated paper products:</td>
+        <td>Coated paper products</td>
         <td>disposable coffee cups, sheets of stickers or address labels, and frozen-food boxes are not recyclable.</td>
       </tr>
       <tr>
-        <td>Most Styrofoam: </td>
+        <td>Most Styrofoam </td>
         <td> styrofoam cups and containers are technically recyclable, but few facilities accept them for cost reasons.</td>
       </tr>
       <tr>
-        <td>Food wrappers:</td>
+        <td>Food wrappers</td>
         <td>candy wrappers, potato chip bags, and plastic wrap cannot be recycled.</td>
       </tr>
       <tr>
-        <td>Food-related paper products:</td>
+        <td>Food-related paper products</td>
         <td>because of the food residue on these products, pizza boxes, take-out containers, napkins, and paper towels cannot be recycled.</td>
       </tr>
 
     </table>
     </section>
-    <section>
-    <h2> Learn About Compact</h2>
-    <p>detail</p>
-   
-
-    <table>
-      <tr>
-        <th>Items</th>
-        <th>Examples</th>
-      </tr>
-      <tr>
-        <td>Plastic</td>
-        <td></td>
-      </tr>
-      <tr>
-        <td>Cardboard</td>
-        <td></td>
-      </tr>
-
-    </table>
-    </section>
-      
-  */}
 
       
       
