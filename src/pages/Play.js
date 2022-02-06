98c38c1a00bdb8fda6a1c80304cb29c6b1e063e0
Nick Raskop <raskop@usc.edu>
2022-02-06 00:28:19 -0800

    Collision logic

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -16,6 +16,36 @@ export default function Play() {
   // });
 
 
+  const spawnTrash = (type) => {
+    console.log('in func');
+    const xpos = (Math.floor(Math.random() * document.body.clientWidth));
+    var trash = document.createElement('div');
+    trash.classList.add('trash');
+    trash.style.left = xpos +"px";
+    document.getElementById("bin-container").appendChild(trash);
+    trash.style.top = "0px"
+    console.log(trash.style.top);
+
+
+  }
+
+
+  function checkCollision(obj1, obj2){
+    console.log('obj2',obj2);
+    var x_collision = (parseInt(obj1.style.left.slice(0,-2)) <= (parseInt(obj2.style.left.slice(0,-2)) + 200)) && ((parseInt(obj1.style.left.slice(0,-2)) + 50) >= parseInt(obj2.style.left.slice(0,-2)));
+    var y_collision = ((parseInt(obj1.style.top.slice(0,-2)) + 50) >= (500 - parseInt(obj2.style.bottom.slice(0,-2))));
+    console.log("X coll 1: " + parseInt(obj1.style.left.slice(0,-2)) + "<="+ (parseInt(obj2.style.left.slice(0,-2)) + 200) + "X coll 2: " + (parseInt(obj1.style.left.slice(0,-2)) + 50) + ">=" + parseInt(obj2.style.left.slice(0,-2)));
+    console.log("Y coll 1: " + (obj1.style.top) + "<=" + (obj2.style.top + obj2.style.height) + "Y coll 2: " + (parseInt(obj1.style.top.slice(0,-2)) + 50) + ">=" + (obj2.style.bottom.slice(0,-2)));
+    // console.log('obj2 bottom:', obj2.style.bottom);
+    if (x_collision && y_collision){
+      obj1.style.visibility ='hidden';
+      console.log('collides');
+    }
+    
+    return x_collision && y_collision;
+  }
+
+
 
 
   const [pos, setPos] = useState(document.body.clientWidth/2 - 100);
@@ -32,6 +62,20 @@ export default function Play() {
     setPos(pos+50);
     console.log('in right');
   }
+
+
+  const fall = () => {
+    console.log('falling');
+    var trashItems = document.getElementsByClassName('trash');
+    console.log(trashItems);
+    for (var i = 0; i < trashItems.length; i += 1){
+      var top = trashItems[i].style.top;
+      top = (parseInt(top.slice(0,-2)) + 10) + "px";
+      trashItems[i].style.top = top;
+      checkCollision(trashItems[i], document.getElementById('recycle'));
+    }
+  }
+  setInterval(fall, 1000);
   
 
 
@@ -39,11 +83,14 @@ export default function Play() {
     // the ' ' char actually represents the space bar key.
     if (e.key === 'a') {
       console.log('a');
+      //console.log(checkCollision(document.getElementById('test'), document.getElementById('trash')));
       left();
     }
     else if (e.key === 'd') {
       console.log('d');
       right()
+    } else if (e.key === '1'){
+      spawnTrash();
     }
   };
 
@@ -63,7 +110,9 @@ export default function Play() {
     <div id='inbody'>
       <h1>Catch the trash in the bin!</h1>
       <div id="bin-container">
-        <div id="recycle" style={{left: pos}}/>
+        <div id="recycle" style={{left: pos, bottom: 100}}/>
+        <div id="test">hey</div>
+        <div id="trash">hi</div>
       </div>
     </div>
   </body>
